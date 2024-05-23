9f37a69c848af6929070e24f6b2fc6fa119d6a71
Nikku4211 <73254369+nikku4211@users.noreply.github.com>
2024-05-23 15:48:54 -0400

    Add files via upload

--- music/BasicDog.h
+++ music/BasicDog.h
@@ -2,7 +2,16 @@
 
 #include <stdint.h>
 //#include <tonc.h>
-#include "../source/advikku.h"
+#include "../source/advikku_global.h"
+#include "zetakick_bin.h"
+#include "zetasnare_bin.h"
 
-extern const uint8_t basicSong[];
-extern const uint16_t basicInstruments[];
+extern const uint8_t * const basicSong[];
+extern const uint8_t basicSongOrder1[];
+extern const uint8_t basicSongOrder2[];
+extern const uint8_t basicSongOrder3[];
+extern const uint8_t basicSongOrder4[];
+extern const uint16_t basicSaw[];
+extern const uint16_t basicSquareInstruments[];
+extern const uint16_t basicWaveInstruments[];
+extern const uint16_t basicNoiseInstruments[];
