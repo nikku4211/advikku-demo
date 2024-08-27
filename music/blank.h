7755fb351fcfcad5f4f58024e20cac005c49cce8
Nikku4211 <73254369+nikku4211@users.noreply.github.com>
2024-08-26 22:54:09 -0400

    Add my music from Sleep Paradox

---
+++ music/blank.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <stdint.h>
+#include <tonc.h>
+#include "../source/advikku_global.h"
+#include "blank_raw.h"
+extern const uint8_t blankSongHeader[];
+extern const uint8_t blankSongOrderBlank[];
+extern const uint8_t * const blankSong[];
+extern const uint16_t blankWaveformBlank[];
+extern const uint16_t blankSquareInstruments[];
+extern const uint16_t blankWaveInstruments[];
+extern const uint16_t * const blankWaveforms[];
+extern const uint16_t blankNoiseInstruments[];
+extern const uint8_t * const blankDMAInstruments[];
+extern const uint16_t blankDMADurations[];
+extern const advikSongStruct blankSongStruct;
